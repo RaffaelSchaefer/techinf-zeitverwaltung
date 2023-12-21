b3535b38f08c12910be686bf813138eed9c1abbb
Raffael Elias Schäfer <s20a3c@umwelt-campus.de>
2023-12-21 14:43:52 +0100

    feat!: Added test script

---
+++ arduino/firmware/firmware.ino
@@ -0,0 +1,34 @@
+#include <SPI.h>      
+#include <MFRC522.h>  
+
+#define RST_PIN     5          
+#define SS_PIN      4 
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+ String last_card = "";
+
+void setup() {
+  Serial.begin(115200);   
+  SPI.begin();      
+  mfrc522.PCD_Init();   
+}
+
+void loop() {
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+  String new_card = "";
+  //Zeigt die UID im serial monitor
+  for (byte i = 0; i < mfrc522.uid.size; i++) {
+     new_card.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     new_card.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  if (new_card != last_card) {
+    last_card = new_card;
+    Serial.println();
+    Serial.print(" UID tag  :"+ new_card);
+    }
+} 
