ecb193e5e8770c47d2ed3051392fcc0d4df50f11
Raffael Elias Schäfer <s20a3c@umwelt-campus.de>
2023-12-25 18:43:59 +0100

    feat!: Added the first path for the REST API
    Added Flask

--- server/main.py
+++ server/main.py
@@ -2,6 +2,8 @@ import os
 import argparse
 import sqlite3
 import click
+import json
+from flask import Flask, request, jsonify
 
 parser = argparse.ArgumentParser(
     prog='Zeitverwaltung Server',
@@ -14,6 +16,8 @@ parser.add_argument('-i', '--init', action='store_true',
                     help="initializes the Server")
 parser.add_argument('-add', '--add_demo_data', action='store_true',
                     help="adds Demo Data to the Project")
+parser.add_argument('-d', '--debug', action='store_true',
+                    help="starts the Server in debug mode")
 
 args = parser.parse_args()
 
@@ -21,8 +25,19 @@ if (os.path.isfile("./db.sqlite") != True):
     db = open('db.sqlite', 'x')
     db.close()
 
-connection = sqlite3.connect("db.sqlite")
-cursor = connection.cursor()
+
+def create_db_connection(db_name="db.sqlite"):
+    return sqlite3.connect("db.sqlite")
+
+
+def create_cursor(connection):
+    return connection.cursor()
+
+
+connection = create_db_connection()
+cursor = create_cursor(connection)
+
+api = Flask(__name__)
 
 
 def init():
@@ -39,11 +54,34 @@ def reset():
     cursor.execute("DROP TABLE users")
     cursor.execute("DROP TABLE cards")
 
+
 def add_demo_data():
     cursor.execute("INSERT INTO cards (UID) VALUES ('037F8B97')")
-    cursor.execute("INSERT INTO users (first_name, last_name) VALUES ('Max', 'Mustermann')")
-    cursor.execute("INSERT INTO card_ownership (user_ID, card_ID) VALUES (1, 1)")
+    cursor.execute(
+        "INSERT INTO users (first_name, last_name) VALUES ('Max', 'Mustermann')")
+    cursor.execute(
+        "INSERT INTO card_ownership (user_ID, card_ID) VALUES (1, 1)")
     connection.commit()
+
+
+@api.route("/users")
+def users():
+    connection = create_db_connection()
+    cursor = create_cursor(connection)
+    cursor.execute("SELECT * FROM users")
+    rows = cursor.fetchall()
+    db_data = {
+        "data": {
+            "users": []
+        }
+    }
+    for row in rows:
+        db_data["data"]["users"].append(
+            {"first_name": row[0], "last_name": row[1]})
+    cursor.close()
+    connection.close()
+    return db_data
+
 
 def main():
     if (args.reset):
@@ -56,6 +94,8 @@ def main():
         add_demo_data()
     cursor.close()
     connection.close()
+    api.run(debug=args.debug)  # Remove Debug for later release
+
 
 if __name__ == "__main__":
     main()
