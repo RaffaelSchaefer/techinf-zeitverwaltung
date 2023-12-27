022c4260fa83ba8bbd2de99a9713a3b5773a45dc
Raffael Elias Schäfer <s20a3c@umwelt-campus.de>
2023-12-27 19:15:04 +0100

    feat!: General improvements

--- arduino/firmware/firmware.ino
+++ arduino/firmware/firmware.ino
@@ -1,34 +1,86 @@
-#include <SPI.h>      
-#include <MFRC522.h>  
+#include <SPI.h>
+#include <MFRC522.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
 
-#define RST_PIN     5          
-#define SS_PIN      4 
+#include ".env/credentials.h"
+#include ".env/api.h"
+
+#define RST_PIN 5
+#define SS_PIN 4
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);
-String last_card = "";
+int scan_delay = 20;
 
-void setup() {
-  Serial.begin(115200);   
-  SPI.begin();      
-  mfrc522.PCD_Init();   
+void setup()
+{
+  Serial.begin(115200);
+  SPI.begin();
+  mfrc522.PCD_Init();
+  delay(10);
+  Serial.println('\n');
+
+  WiFi.begin(SSID, PASSWORD); // Connect to the network
+  Serial.println("Connecting to " + String(SSID) + " ...");
+
+  for (int i = 1; WiFi.status() != WL_CONNECTED; i++)
+  {
+    delay(1000);
+    Serial.print(String(i) + " ");
+  }
+
+  Serial.print("\nConnection established!\nIP address:\t");
+  Serial.println(WiFi.localIP());
 }
 
-void loop() {
-  if ( ! mfrc522.PICC_IsNewCardPresent()) {
-    return;
+void loop()
+{
+  if (scan_delay >= 20)
+  {
+    scan_delay = 0;
+    if (!mfrc522.PICC_IsNewCardPresent())
+      return;
+    if (!mfrc522.PICC_ReadCardSerial())
+      return;
+    String uid = "";
+    for (byte i = 0; i < mfrc522.uid.size; i++)
+      uid.concat(String(mfrc522.uid.uidByte[i], HEX));
+    Serial.print("\nUID: " + uid);
+    if (WiFi.status() == WL_CONNECTED)
+    {
+      WiFiClient client;
+      HTTPClient http;
+
+      String serverPath = String(API_ADDRESS) + "/log";
+
+      http.begin(client, serverPath.c_str());
+      http.addHeader("Content-Type", "application/json");
+
+      int httpResponseCode = http.POST("{\"data\":{\"UID\": \"" + uid + "\"}}");
+
+      if (httpResponseCode > 0)
+      {
+        Serial.print("\nHTTP Response code: ");
+        Serial.println(httpResponseCode);
+      }
+      else
+      {
+        Serial.print("\nError code: ");
+        Serial.println(httpResponseCode);
+        String payload = http.getString();
+        Serial.println(payload);
+      }
+      http.end();
+    }
+    else
+    {
+      Serial.println("Error: WiFi Disconnected");
+    }
   }
-  if ( ! mfrc522.PICC_ReadCardSerial()) {
-    return;
+  else
+  {
+    delay(10);
+    scan_delay++;
   }
-  String new_card = "";
-  //Zeigt die UID im serial monitor
-  for (byte i = 0; i < mfrc522.uid.size; i++) {
-    new_card.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-    new_card.concat(String(mfrc522.uid.uidByte[i], HEX));
-  }
-  if (new_card != last_card) {
-    last_card = new_card;
-    Serial.println();
-    Serial.print(" UID tag  :"+ new_card);
-  }
-} 
+}
