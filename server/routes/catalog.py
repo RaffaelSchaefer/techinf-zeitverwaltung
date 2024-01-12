e908d36feb78af9d0728588e4c7db1db6a25ba33
Raffael Elias Schäfer <s20a3c@umwelt-campus.de>
2024-01-12 12:18:01 +0100

    feat!: Added more pages

--- server/routes/catalog.py
+++ server/routes/catalog.py
@@ -9,88 +9,93 @@ api = Flask(__name__, template_folder="../template", static_folder="../static")
 api.jinja_env.add_extension('pypugjs.ext.jinja.PyPugJSExtension')
 
 
+@api.errorhandler(404)
+def not_found():
+    return render_template("404.pug", title="Page not found")
+
+
 @api.route("/")
 def index():
     return redirect(url_for('site'))
 
 
-@api.errorhandler(404)
-def not_found(e):
-    return render_template("404.pug", title="Page not found")
-
-
 @api.route("/site")
 def site():
     try:
-        return render_template('index.pug', title="Home", user_data=user_list())
+        return render_template('index.pug', title="Home")
     except Exception as e:
         return render_template('error.pug', title="Error", code=400, msg=str(e))
 
 
 @api.route("/users")
 def users():
-    return user_list()
+    try:
+        return render_template('users.pug', title="All Users", user_data=user_list())
+    except Exception as e:
+        return render_template('error.pug', title="Error", code=400, msg=str(e))
 
 
 @api.route("/user/<user_id>")
 def user(user_id):
-    return user_detail(user_id)
+    try:
+        return render_template('user_detail.pug', title="User Details", user_data=user_detail(user_id))
+    except Exception as e:
+        return render_template('error.pug', title="Error", code=400, msg=str(e))
 
 
-@api.route("/create-user", methods=["POST"])
+@api.route("/create-user", methods=['GET', 'POST'])
 def create_user():
-    if request.method == "POST":
-        try:
-            data = request.get_json()
-            if check_key(data["key"]) is False:
-                return {"error": "Incorrect API Key"}, 401
-            user_create(data["data"]["first_name"], data["data"]["last_name"])
-            return jsonify(data), 201
-        except Exception as e:
-            return {
-                "error": str(e)
-            }, 400
+    try:
+        if request.method == "POST":
+            data = request.form
+            user_create(data["first_name"], data["last_name"])
+            return redirect(url_for('users'))
+        elif request.method == "GET":
+            return render_template('create_user.pug', title="Create User")
+    except Exception as e:
+        return render_template('error.pug', title="Error", code=400, msg=str(e))
 
 
 @api.route("/cards")
 def cards():
-    return card_list()
+    try:
+        return render_template('cards.pug', title="All Cards", card_data=card_list())
+    except Exception as e:
+        return render_template('error.pug', title="Error", code=400, msg=str(e))
 
 
 @api.route("/card/<card_id>")
 def card(card_id):
-    return card_detail(card_id)
+    try:
+        return render_template('card_detail.pug', title="Card Details", card_data=card_detail(card_id))
+    except Exception as e:
+        return render_template('error.pug', title="Error", code=400, msg=str(e))
 
 
-@api.route("/create-card", methods=["POST"])
+@api.route("/create-card", methods=["Get", "POST"])
 def create_card():
-    if request.method == "POST":
-        try:
-            data = request.get_json()
-            if check_key(data["key"]) is False:
-                return {"error": "Incorrect API Key"}, 401
-            card_create(data["data"]["UID"])
-            return jsonify(data), 201
-        except Exception as e:
-            return {
-                "error": str(e)
-            }, 400
+    try:
+        if request.method == "POST":
+            data = request.form
+            card_create(data["UID"])
+            return redirect(url_for('cards'))
+        elif request.method == "GET":
+            return render_template('create_card.pug', title="Register new Card")
+    except Exception as e:
+        return render_template('error.pug', title="Error", code=400, msg=str(e))
 
 
-@api.route("/grant-ownership", methods=["POST"])
+@api.route("/grant-ownership", methods=["GET", "POST"])
 def grant_ownership():
-    if request.method == "POST":
-        try:
-            data = request.get_json()
-            if check_key(data["key"]) is False:
-                return {"error": "Incorrect API Key"}, 401
-            grant_card_ownership(
-                data["data"]["user_ID"], data["data"]["card_UID"])
-            return jsonify(data), 201
-        except Exception as e:
-            return {
-                "error": str(e)
-            }, 400
+    try:
+        if request.method == "POST":
+            data = request.form
+            grant_card_ownership(data["user_ID"], data["card_UID"])
+            return redirect(url_for('user', user_id=data["user_ID"]))
+        elif request.method == "GET":
+            return render_template('grant_ownership.pug', title="Grant Ownership", user_data=user_list(), card_data=card_list())
+    except Exception as e:
+        return render_template('error.pug', title="Error", code=400, msg=str(e))
 
 
 @api.route("/log", methods=["POST"])
