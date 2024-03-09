695d2139b3f90c0ebf9af939754cb34c7571669b
Raffael Elias Schäfer <s20a3c@umwelt-campus.de>
2024-03-09 16:11:44 +0100

    Aktualisiere API-Schlüssel in der Simulator-Datei

--- server/simulator.py
+++ server/simulator.py
@@ -7,10 +7,8 @@ from controller import card_list
 
 cards = card_list()["data"]["cards"]
 UIDs = [card["UID"] for card in cards]
-#key = os.getenv('API_KEY')
-key = "bRBk9qircBeL0vegXBbgVAaPTBE96UqeWcC65evRCkW5oegVzjx7AmYD7uAtT8o2"
+key = os.getenv('API_KEY')
 
 while True:
     r = requests.post("http://localhost:9000/log", json={"key": key, "data": {"UID": random.choice(UIDs)}})
-    print(r.status_code, r.reason)
-    time.sleep(15)
+    time.sleep(60)
